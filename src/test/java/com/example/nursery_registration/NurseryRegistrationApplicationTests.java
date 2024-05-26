1c5043f18fa1c2426ec16e94fd4c25c3e4d11649
Pedro Silva <pedro.silva@b2ml.com.br>
2024-05-26 15:23:52 -0300

    feat: add spring inizilr files

---
+++ src/test/java/com/example/nursery_registration/NurseryRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nursery_registration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NurseryRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
